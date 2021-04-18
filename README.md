c62bc3bc948260aa26bbc5f6102437995fb9f534
Moishi Netzer <moishinetzer@gmail.com>
2021-04-18 14:43:44 +0100

    Added logos and fixed top margin

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# mnetzer.com
-Created with CodeSandbox
+# mnetzer.com Landing Page
+
+Created using React with Tailwind CSS.
+
+First Tailwind project. This was created for two reasons. Both to have a starting portfolio page (somewhat) and to also use this as a way to learn how to use Tailwind CSS
+
+So far its been great and enjoyable to learn I higly suggest it.
+
+https://tailwindcss.com/
+
+I learned it using the following course which I highly recommend
+
+https://coderstape.com/series/13-learn-tailwind-css-from-scratch
--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import { React, useState } from "react";
 import "./styles.css";
+import github from "../public/github.png";
+import linkedin from "../public/linkedin.png";
+import gmail from "../public/gmail.png";
 
 export default function App() {
   var [mainBg, setMainBg] = useState("to-gray-700");
 
-  function setBg(string) {
-    console.log(string);
-  }
-
   return (
     <div
-      className={`App h-screen w-screen flex items-center justify-center flex-wrap font-poppins font-bold tracking-wide bg-gradient-to-br from-gray-900 ${mainBg}`}
+      className={`App h-screen w-screen flex items-start justify-center font-poppins font-bold tracking-wide bg-gradient-to-br from-gray-900 ${mainBg}`}
     >
       <div>
-        <p className="bg-clip-text text-transparent bg-gradient-to-l from-blue-400 to-red-400 w-screen text-4xl sm:text-7xl">
+        <div className={"flex justify-center space-x-8 mt-8"}>
+          <a href={"https://github.com/moishinetzer/"}>
+            <img src={github} alt="GitHub Link" className={"h-12 sm:h-20"} />
+          </a>
+          <a href={"https://www.linkedin.com/in/marc-netzer-03719b147/"}>
+            <img
+              src={linkedin}
+              alt="LinkedIn Link"
+              className={"h-12 sm:h-20"}
+            />
+          </a>
+          <a href={"mailto:moishinetzer@gmail.com"}>
+            <img src={gmail} alt="Gmail Link" className={"h-12 sm:h-20"} />
+          </a>
+        </div>
+        <p className="bg-clip-text text-transparent bg-gradient-to-l from-blue-400 to-red-400 w-screen text-4xl sm:text-7xl mt-16">
           Moishi Netzer
         </p>
         <p className="bg-clip-text text-transparent bg-gradient-to-l from-gray-400 italic text-2xl sm:text-5xl p-10 sm:p-16 font-normal">
