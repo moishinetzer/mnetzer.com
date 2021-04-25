72c3be832c7a1dd9457ffb9da18401747557d6c2
Moishi Netzer <moishinetzer@gmail.com>
2021-04-25 18:09:18 +0100

    analytics maybe

--- src/App.js
+++ src/App.js
@@ -30,7 +30,6 @@ export default function App() {
     } else {
       auth.signInAnonymously();
     }
-    firebase.analytics();
   }, [auth]);
 
   return (
--- src/LogoLink.js
+++ src/LogoLink.js
@@ -1,3 +1,4 @@
+import firebase from "firebase/app";
 export default function LogoLink(props) {
   const { href, src, alt } = props;
   return (
@@ -8,6 +9,9 @@ export default function LogoLink(props) {
         ease-out
         duration-150
         hover:opacity-70"
+      onClick={() => {
+        firebase.analytics().logEvent(alt);
+      }}
     >
       <img src={src} alt={alt} className={"h-12 sm:h-20"} />
     </a>
