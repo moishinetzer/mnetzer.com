088509e4ceaf1a8292c5f088ed0bdb4c6446b561
Moishi Netzer <moishinetzer@gmail.com>
2021-04-25 15:07:31 +0100

    changestyle

--- src/ThumbsUp.js
+++ src/ThumbsUp.js
@@ -1,6 +1,7 @@
 import firebase from "firebase";
 import { thumbsUpDb } from "./thumbsUpDb";
 import { useState } from "react";
+import { Transition } from "@headlessui/react";
 
 export default function ThumbsUp(props) {
   const { color, canVoteFunctions } = props;
@@ -8,8 +9,6 @@ export default function ThumbsUp(props) {
 
   var [chosen, setChosen] = useState(false);
   var [count, setCount] = useState();
-
-  var pointer = canVote ? "cursor-pointer" : "fg";
 
   thumbsUpDb
     .ref("colors")
@@ -39,7 +38,7 @@ export default function ThumbsUp(props) {
         className={`
           ${chosen ? "text-gray-200" : "text-gray-400"} 
           ${canVote && "cursor-pointer hover:opacity-60"} 
-          h-8 w-8 sm:h-9 sm:w-9 stroke-current 
+          h-8 w-8 sm:h-10 sm:w-10 stroke-current 
           transition-opacity
           duration-150 ease-out  
           pb-1 sm:pb-0`}
@@ -54,15 +53,19 @@ export default function ThumbsUp(props) {
           d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
         />
       </svg>
-      <>
-        {!canVote && (
-          <div
-            className={` ${chosen ? "text-gray-200 text-xl" : "text-gray-400"}`}
-          >
-            {count}
-          </div>
-        )}
-      </>
+      <Transition
+        appear={false}
+        show={!canVote}
+        enter="duration-1000"
+        enterFrom="transform scale-0"
+        enterTo="transform scale-100"
+      >
+        <div
+          className={`${chosen ? "text-gray-200" : "text-gray-400"} sm:text-xl`}
+        >
+          {count}
+        </div>
+      </Transition>
     </div>
   );
 }
